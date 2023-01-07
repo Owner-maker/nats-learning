c880d62f8796c9a8a3bd3422f8584bb9b36ac429
Owner-maker <lisitsyn.artem.d@gmail.com>
2023-01-07 06:18:47 +0400

    Change publisher

--- cmd/publisher/main.go
+++ cmd/publisher/main.go
@@ -3,34 +3,51 @@ package main
 import (
 	"github.com/nats-io/stan.go"
 	"github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
-	"nats-learning/configs"
+	"io"
+	"nats-learning/internal/util"
+	"os"
 )
 
-func initConfig() error {
-	viper.AddConfigPath("configs")
-	viper.SetConfigName("config")
-	return viper.ReadInConfig()
-}
-
 func main() {
-	config, err := configs.LoadConfig(".")
+	config, err := util.LoadConfig(".")
 	if err != nil {
 		logrus.Fatalf("error while initializing config file: %s", err.Error())
 	}
 	logrus.Print("successfully initialized config file")
 
-	// connect to nats streaming server
-	sc, err := stan.Connect(config.ClusterId, config.ClientProducer, stan.NatsURL(config.NatsUrl))
+	// connect to the nats streaming server
+	sc, err := stan.Connect(
+		config.ClusterId,
+		config.ClientProducer,
+		stan.NatsURL(config.NatsUrl))
 	if err != nil {
-		logrus.Fatalf("error while connnecting to nats streaming server: %s", err.Error())
+		logrus.Fatalf("error while connnecting to the nats streaming server: %s", err.Error())
 	}
 	defer func(sc stan.Conn) {
 		err := sc.Close()
 		if err != nil {
-			logrus.Fatalf("error while closing publisher connection to nats streaming server: %s", err.Error())
+			logrus.Fatalf("error while closing publisher connection to the nats streaming server: %s", err.Error())
 		}
 	}(sc)
-	logrus.Print("successfully connected to nats streaming server")
+	logrus.Print("successfully connected to the nats streaming server")
 
+	// parse static json file
+	dataJson, err := os.Open(config.JsonStaticModelPath)
+	if err != nil {
+		logrus.Fatalf("error while opening json file: %s", err.Error())
+	}
+	defer func(dataJson *os.File) {
+		err = dataJson.Close()
+		if err != nil {
+			logrus.Fatalf("error while closing json fie: %s", err.Error())
+		}
+	}(dataJson)
+	byteValue, _ := io.ReadAll(dataJson)
+
+	// send json static data to the nats streaming server
+	err = sc.Publish(config.NatsSubject, byteValue)
+	if err != nil {
+		logrus.Fatalf("error while publishing json static file to the nats streaming server: %s", err.Error())
+	}
+	logrus.Print("successfully published json static file to the nats streaming server")
 }
